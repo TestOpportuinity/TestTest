433b96bb102cf92243fdb87ac5dc93c3758460e4
Tomasz Omelczuk <tomaszome@gmail.com>
2018-06-08 15:07:00 +0200

    Update library.sql

--- library.sql
+++ library.sql
@@ -62,7 +62,7 @@ INSERT INTO `author` (`ID`, `firstName`, `lastName`) VALUES
 (23, 'Anil', 'Hemrajani'),
 (24, 'Noel', 'Kalicharan');
 (25, 'Bruce', 'Dang');
-
+(30, 'Dawid', 'Farbaniec');
 -- --------------------------------------------------------
 
 --
@@ -93,8 +93,9 @@ INSERT INTO `book` (`ISBN`, `CATEGORY_ID`, `BOOK_TITLE`, `AUTHOR_ID`, `PUBLISHER
 ('978-83-246-9806-6', 3, 'Java. Projektowanie aplikacji WWW', 1, 'Helion'),
 ('978-83-283-0253-2', 4, 'ABC Linux. Wydanie II', 2, 'Helion'),
 ('978-83-283-1250-0', 1, 'Black Hat Python. Język Python dla hakerów i pentesterów', 11, 'Helion');
-('978-83-283-0678-3', 7, 'Inżynieria odwrotna w praktyce. Narzędzia i techniki', 25, 'Helion);
- ('978-83-283-0678-3', 7, 'Inżynieria odwrotna w praktyce. Narzędzia i techniki 2', 25, 'Helion);
+('978-83-283-0678-3', 7, 'Inżynieria odwrotna w praktyce. Narzędzia i techniki', 25, 'Helion');
+ ('978-83-283-0678-3', 7, 'Inżynieria odwrotna w praktyce. Narzędzia i techniki 2', 25, 'Helion');
+ ('978-83-246-8862-3', 7, 'Techniki Twórców złośliwego oprogamowania. Elementarz programisty', 30, 'Helion');
 
 -- --------------------------------------------------------
 
